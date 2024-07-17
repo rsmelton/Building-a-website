def613187899c081ae4e27a05613207dbcf753c3
Robert Melton <rsmelton@crimson.ua.edu>
2024-07-17 18:30:18 -0500

    Small bug fix

--- building-a-website/src/Components/Navbar.js
+++ building-a-website/src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                // <a href="#" className="navbar-logo">Dragon's Brew Coffee</a>
+                {/* <a href="#" className="navbar-logo">Dragon's Brew Coffee</a> */}
                 <img src={dragonsBrewLogoImage} alt='logo image' width={130} height={80}/>
                 <ul className="navbar-menu">
                     <li className="navbar-item"><a href="#">Home</a></li>
