543bd4d410ac34d9f490a4180816f2a2b229c92f
Robert Melton <rsmelton@crimson.ua.edu>
2024-07-26 20:03:05 -0500

    Added prices to all the drinks

--- building-a-website/src/Components/Menu.js
+++ building-a-website/src/Components/Menu.js
@@ -17,49 +17,49 @@ const Menu = () => {
         <div className='menu-container'>
           <div className='cell-container'>
             <img className='cell-item' src={dragonsFireLatte} alt='menu item' />
-            <h3>Dragons Fire Latte</h3>
+            <h3>Dragons Fire Latte - $7.95</h3>
             <p>A spicy twist on a classic latte with a hint of cinnamon and cayenne pepper, topped with a good amount of whipped cream</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={enchantedEspresso} alt='menu item' />
-            <h3>Enchanted Espresso</h3>
+            <h3>Enchanted Espresso - $4.95</h3>
             <p> A double shot of our finest espresso, served with a twist of lemon and a sprinkling of enchanted edible glitter upon request</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={frostDragonLatte} alt='menu item' />
-            <h3>Frost Dragon Latte</h3>
+            <h3>Frost Dragon Latte - $6.95</h3>
             <p>A freezing cold latte with mint and white chocolate with some whipped cream on top and a drizzle of caramel</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={mysticMatcha} alt='menu item' />
-            <h3>Mystic Matcha</h3>
+            <h3>Mystic Matcha - $7.99</h3>
             <p>A vibrant green matcha latte with a touch of vanilla and honey, served with a side of mochi</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={phoenixFrappuccino} alt='menu item' />
-            <h3>Phoenix Frappuccino</h3>
+            <h3>Phoenix Frappuccino - $7.95</h3>
             <p>A cool, blended drink with flavors of mango, raspberry, and a hint of chili, topped with whipped cream and a fiery chocolate drizzle</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={sorcerersSmoresMocha} alt='menu item' />
-            <h3>Sorcerers Smores Mocha</h3>
+            <h3>Sorcerers Smores Mocha - $8.95</h3>
             <p>A decadent mocha with toasted marshmallow, graham cracker crumbles, and chocolate drizzle</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={unicornColdBrew} alt='menu item' />
-            <h3>Unicorn Cold Brew</h3>
+            <h3>Unicorn Cold Brew - $3.95</h3>
             <p>A magical cold brew coffee infused with lavender and vanilla, served with a swirl of pastel-colored cream on the top</p>
             <button classname='order-button'>Add to order</button>
           </div>
           <div className='cell-container'>
             <img className='cell-item' src={wizardsWhiteChcolateMocha} alt='menu item' />
-            <h3>Wizards White Chcolate Mocha</h3>
+            <h3>Wizards White Chcolate Mocha - $6.95</h3>
             <p>A creamy white chocolate mocha mixed with a hazelnut mocha topped with whipped cream and a drizzle of caramel and chocolate</p>
             <button classname='order-button'>Add to order</button>
           </div>
